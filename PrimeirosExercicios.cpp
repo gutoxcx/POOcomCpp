0e5b9eba3dc8c8c55ebc222677ac567c0d8ab929
João Augusto <joaoaugustoxcx@gmail.com>
2025-10-21 10:36:18 -0300

    update

---
+++ PrimeirosExercicios.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //ler um numero inteiro e informa se é par ou ímpar;
+    int numero;
+    cin >> numero;
+    if (numero % 2 == 0) {
+        cout << "o numero digitado é par." << endl;
+    } else {
+        cout << "o numero digitado é impar" << endl;
+    }
+
+    // ler notas ate que o usuario digite -1
+
+    double entradaNotas, somaNotas = 0;
+    double contagemNotas;
+
+    while (true) {
+        cout << "digite sua nota: ";
+        cin >> entradaNotas;
+        if (entradaNotas == -1) {
+            break;
+        }
+        
+        somaNotas+= entradaNotas;
+        contagemNotas++;
+    }
+
+    if (count > 0) {
+        cout << "Média das notas: " << (somaNotas / contagemNotas) << endl;
+    } else {
+        cout << "Nenhuma nota foi digitada" << endl;
+    }
+
+    return 0;
+}
