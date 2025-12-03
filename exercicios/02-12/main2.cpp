d32030f8571d413e70f283a57fd0b1a55517ad16
gutoxcx <joaoaugustoxcx@gmail.com>
2025-12-03 12:13:35 -0300

    02-12

--- exercicios/02-12/main2.cpp
+++ exercicios/02-12/main2.cpp
@@ -1,19 +1,39 @@
-#include <iostream>;
+#include <iostream>
 using namespace std;
 
 /*
 Leia 8 numeros inteiros para dentro de um vetor e determine:
-	O maior valor e em qual posição ele está
-	O menor valor e em qual posiçã ele está
+    O maior valor e em qual posição ele está
+    O menor valor e em qual posição ele está
 */
 
 int main()
 {
-	int vetor[8];
-	int entradaVet;
-	
-	cout << "Digite
-	for (int i = 0; i < 8; i++) {
-		vetor[i] = entradaVet;
-	}
+    int vetor[8];
+    
+    cout << "Digite 8 números inteiros:\n";
+    for (int i = 0; i < 8; i++) {
+        cin >> vetor[i];
+    }
+
+    int maior = vetor[0];
+    int posicaoMaior = 0;
+    int menor = vetor[0];
+    int posicaoMenor = 0;
+
+    for (int i = 1; i < 8; i++) {
+        if (vetor[i] > maior) {
+            maior = vetor[i];
+            posMaior = i;
+        }
+        if (vetor[i] < menor) {
+            menor = vetor[i];
+            posMenor = i;
+        }
+    }
+
+    cout << "\nMaior valor: " << maior << " na posição " << posMaior << endl;
+    cout << "Menor valor: " << menor << " na posição " << posMenor << endl;
+
+    return 0;
 }
